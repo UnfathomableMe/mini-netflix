9d27476bfe4a22e93e01b8603e34b6c072dbb1bd
Neha Shende <54766093+UnfathomableMe@users.noreply.github.com>
2024-01-17 17:40:03 -0500

    Update App.js
    added route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { useSearchParams } from 'react-router-dom';
 import './App.scss'
 import Landing from './components/landing';
 import MovieDetails from './components/details';
 
+
 function App() {
   const [movielist, setMovies] = useState([]);
+  // const [searchParams] = useSearchParams();
 
 const movieDetails = async () =>{
   const omdb_api = 'http://www.omdbapi.com/?apikey=4845c445&s=harry'
@@ -29,7 +32,8 @@ useEffect(()=>{
         <Routes>
           <Route path='/' element={<Landing movielist={movielist}></Landing>}></Route>
           <Route path='/movie/details/:imdbID' element={<MovieDetails></MovieDetails>}></Route>
-          {/* <Route path='/movie/details?movieId=:imdbID' element={<MovieDetails></MovieDetails>}></Route> */}
+          <Route path="/movie/details" element={<MovieDetails/>} />
+          {/* <Route path='/movie/url-details/:imdbID' element={<UrlDetails searchParams={searchParams}></UrlDetails>}></Route> */}
         </Routes>
           
         </div>
